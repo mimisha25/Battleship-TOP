78c356e80541f9071440bc750f3859b0e9d7f438
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-07 13:35:59 -0500

    Add isSunk method in Ship class. It will give the answer if the whole ship is sunk or not

--- src/ship.js
+++ src/ship.js
@@ -9,6 +9,16 @@ export class Ship {
 
     hit() {
         this.hits += 1;
+        this.isSunk();
 
+    }
+
+    isSunk() {
+        if (this.hits >= this.length) {
+            this.sunk = true;
+
+        }
+        else this.sunk = false;
+        return this.sunk;  // Return whether the ship is sunk
     }
 }
