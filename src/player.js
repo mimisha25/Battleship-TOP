e590586f20bc49dcb1be23f20c11c4250ce79b3b
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-12 14:18:30 -0500

    Add method attack which will transfer coordinates of attack cells to the gameboard receiveAttack method

--- src/player.js
+++ src/player.js
@@ -3,4 +3,10 @@ export class Player {
         this.name = name;
         this.gameboard = gameboard;
     }
+
+    attack(opponent, x, y) {
+        if (x < 0 || x > 10 || y < 10 || y >= 10) throw new Error("Coordinates out of the bounds!");
+        const result = opponent.gameboard.receiveAttack(x, y);
+        return result;
+    }
 }
