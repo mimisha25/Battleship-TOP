c7db2eb42298bc3a1cc2d2c4d830333de68e2419
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-13 15:25:43 -0500

    Add method for RealPlayer when is attack the dom will change and it will check if all ships are sunk or not

--- src/realPlayer.js
+++ src/realPlayer.js
@@ -6,4 +6,21 @@ export class RealPlayer extends Player {
     constructor(name, gameboard) {
         super(name, gameboard);
     }
+    attack(opponent, x, y) {
+        if (x < 0 || x >= 10 || y < 0 || y >= 10) throw new Error("Coordinates out of bounds!");
+        const result = super.attack(opponent, x, y);
+        const cell = document.querySelector(`#computer-board [data-row="${x}"][data-col="${y}"]`);
+        if (result === 'hit') {
+            message.textContent = "You hit the ship";
+            cell.classList.add('hit');
+        } else if (result === 'miss') {
+            message.textContent = "You missed the attack";
+            cell.textContent = "X";
+            cell.classList.add('miss');
+        }
+        if (opponent.gameboard.allShipsSunk()) {
+            console.log("All ships are sunk!");
+            message.textContent = "You win! All enemy ships are sunk";
+        } return result;
+    }
 }
