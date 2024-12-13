5bc5da1946a79f34620f789f7663d8e2b47f37b6
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-13 15:32:38 -0500

    Add randomAttack method so computer will attack on random cells of opponent's board

--- src/computerPlayer.js
+++ src/computerPlayer.js
@@ -6,4 +6,30 @@ export class ComputerPlayer extends Player {
     constructor(name, gameboard) {
         super(name, gameboard);
     }
+
+    randomAttack(opponent) {
+        setTimeout(() => {
+            let x, y;
+            do {
+                x = Math.floor(Math.random() * 10);
+                y = Math.floor(Math.random() * 10);
+            } while (opponent.gameboard.board[x][y].hit);
+
+            const result = opponent.gameboard.receiveAttack(x, y);
+            console.log(`Computer attacks at [${x}, ${y}] and it was a ${result}`);
+            const cell = document.querySelector(`[data-row="${x}"][data-col="${y}"]`);
+            if (result === 'hit') {
+                message.textContent = "Computer hit your ship";
+                cell.classList.add('hit');
+            } else {
+                message.textContent = "Computer missed the attack";
+                cell.textContent = "X";
+                cell.classList.add('miss');
+            }
+            if (opponent.gameboard.allShipsSunk()) message.textContent = "Computer wins! All your ships are sunk.";
+            return { result, x, y };
+        }, 2000);
+    };
+
+
 }
