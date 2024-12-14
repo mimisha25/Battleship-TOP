9afa93a44f84c64ceade1e00498cb20c2aa7d519
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-14 17:22:20 -0500

    Create handleDragStart.js which gets chosen ship and drag all over the shipsContainer and target the place on your's board

---
+++ src/dom/handleDragStart.js
@@ -0,0 +1,10 @@
+export function handleDragStart(event, ship) {
+    event.dataTransfer.setData("text/plain");
+    event.target.classList.add("dragging");
+    event.target.ship = ship;
+    const shipClone = ship.cloneNode(true);
+    shipClone.classList.add("dragging-ship");
+    document.body.appendChild(shipClone);
+    event.dataTransfer.setDragImage(shipClone, 0, 0);
+    event.target.shipClone = shipClone;
+}
