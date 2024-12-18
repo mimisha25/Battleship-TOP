ffe53dcb3e9756d3fdb869bd4d245f19298c5aac
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-18 11:32:01 -0500

    Fix the bug: add variable for playerGameboard

--- src/dom/set.js
+++ src/dom/set.js
@@ -1,6 +1,6 @@
 import { Ship } from "../ship.js";
 import { Gameboard } from "../gameboard.js";
-
+export const playerGameboard = new Gameboard("player1-board", false);
 export function setPlayerShips(playerShips) {
     playerShips = playerShips.map(ship => {
         const shipInstance = new Ship(ship.size);
