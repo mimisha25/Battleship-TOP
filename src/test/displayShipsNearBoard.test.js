72e7aef7bbf506289f4593445c59b9d747330a98
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-20 12:31:26 -0500

    Add testing for displayShipsNearBoard- add first unit test- should create ship rows and cells with correct size

---
+++ src/test/displayShipsNearBoard.test.js
@@ -0,0 +1,28 @@
+import { displayShipsNearBoard } from '../dom/displayShipsNearBoard.js'; // Adjust path if necessary
+import * as handleDragStartModule from '../dom/handleDragStart.js';
+import * as handleDragEndModule from '../dom/handleDragEnd.js';
+
+jest.mock('../dom/handleDragStart.js');
+jest.mock('../dom/handleDragEnd.js');
+
+describe('displayShipsNearBoard', () => {
+    let shipsContainer;
+
+    beforeEach(() => {
+        document.body.innerHTML = '<div id="ships-container"></div>';
+        shipsContainer = document.getElementById('ships-container');
+    });
+
+    test('should create ship rows and cells with correct size', () => {
+        displayShipsNearBoard();
+        const header = shipsContainer.querySelector('h3');
+        expect(header).not.toBeNull();
+        expect(header.innerText).toBe('Please, drag and drop ships on your board');
+        const shipRows = shipsContainer.querySelectorAll('.ship-row');
+        expect(shipRows.length).toBe(5);
+        shipRows.forEach((shipRow, index) => {
+            const shipCells = shipRow.querySelectorAll('.ship-cell');
+            expect(shipCells.length).toBe(index + 1);
+        });
+    });
+});
