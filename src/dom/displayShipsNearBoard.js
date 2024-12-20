bcee32692fba14436e1dbc2e47c93abd826df22f
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-20 12:16:55 -0500

    Fix the bug innerHTML too innerText

--- src/dom/displayShipsNearBoard.js
+++ src/dom/displayShipsNearBoard.js
@@ -4,14 +4,12 @@ export function displayShipsNearBoard() {
     const shipsContainer = document.getElementById("ships-container");
     const shipSizes = [1, 2, 3, 4, 5];
     const h3 = document.createElement("h3");
-    h3.innerHTML = "Please, drag and drop ships on your board";
+    h3.innerText = "Please, drag and drop ships on your board";
     shipsContainer.appendChild(h3);
-
     shipSizes.forEach((size, index) => {
         const ship = document.createElement("div");
         ship.classList.add("ship-row");
-        ship.dataset.shipSizes = size;
-
+        ship.dataset.shipSize = size;
         for (let i = 0; i < size; i++) {
             const shipCell = document.createElement("div");
             shipCell.classList.add("ship-cell");
@@ -23,5 +21,5 @@ export function displayShipsNearBoard() {
             ship.appendChild(shipCell);
         }
         shipsContainer.appendChild(ship);
-    })
+    });
 }
