bae52800f05a685db8c9693396824fe539696417
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-21 13:09:42 -0500

    Add unit test for handleDragEnd-should remove the shipClone from the document body

---
+++ src/test/handleDragEnd.test.js
@@ -0,0 +1,24 @@
+import { handleDragEnd } from "../dom/handleDragEnd.js";
+
+describe("handleDragEnd", () => {
+    let event, target, shipClone, ship;
+
+    beforeEach(() => {
+        target = document.createElement("div");
+        target.classList.add("dragging");
+        shipClone = document.createElement("div");
+        shipClone.classList.add("ship-clone");
+        target.shipClone = shipClone;
+        ship = document.createElement("div");
+        ship.classList.add("ship");
+        target.ship = ship;
+        document.body.appendChild(shipClone);
+        document.body.appendChild(ship);
+        event = { target };
+    });
+
+    test("should remove the shipClone from the document body", () => {
+        handleDragEnd(event);
+        expect(document.body.contains(shipClone)).toBe(false);
+    });
+})
