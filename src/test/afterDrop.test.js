ab11fdcb911b4f4409f8a9bafe9b2254a2ee4def
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-28 15:38:04 -0500

    Add unit testing for afterDrop - should update the DOM and call setPlayerShips when playerShips length is 5

---
+++ src/test/afterDrop.test.js
@@ -0,0 +1,30 @@
+import { afterDrop } from "../dom/afterDrop.js";
+import { setPlayerShips } from "../dom/set.js";
+
+jest.mock("../dom/set.js");
+
+describe("afterDrop", () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        document.body.innerHTML = `
+        <div id="ships-container">
+          <h3>Place your ships</h3>
+          <div id="vertical"></div>
+          <div id="horizontal"></div>
+        </div>
+        <div id="message"></div>
+      `;
+    });
+
+    test("should update the DOM and call setPlayerShips when playerShips length is 5", () => {
+        const playerShips = [1, 2, 3, 4, 5];
+        afterDrop(playerShips);
+        expect(document.querySelector("h3")).toBeNull();
+        expect(document.querySelector("#vertical")).toBeNull();
+        expect(document.querySelector('#horizontal')).toBeNull();
+        expect(document.querySelector('#message').textContent).toBe("Now start attack the computer's board");
+        expect(setPlayerShips).toHaveBeenCalledTimes(1);
+        expect(setPlayerShips).toHaveBeenCalledWith(playerShips);
+    });
+})
