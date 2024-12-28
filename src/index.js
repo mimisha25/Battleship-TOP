25abf57af6c02b89bf56558b4cb10f573a593922
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-12-28 16:12:47 -0500

    Import all module in main js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+import { createGameboardElement } from "./dom/createGameboardElement.js";
+import { displayShipsNearBoard } from "./dom/displayShipsNearBoard.js";
+import { placeComputerShips } from "./dom/placeComputerShips.js";
+import { computerGameboard } from "./dom/setComp.js";
+import { playerGameboard } from "./dom/set.js";
+import { addAttackEventListeners } from "./addAttackEventListeners.js";
+import { initializeEventListeners } from "./dom/orientation.js";
+createGameboardElement('player1-board');
+createGameboardElement('computer-board', true);
+placeComputerShips();
+displayShipsNearBoard();
+addAttackEventListeners();
+initializeEventListeners()
+console.log(playerGameboard);
+console.log(computerGameboard);
